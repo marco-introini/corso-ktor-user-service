bb5282abd47b85850cce8e7053e37f1fc435b58d
Marco "Mint" Introini <mint.dev@pm.me>
2025-03-07 10:39:53 +0100

    First Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "corso-ktor-user-service"
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package it.marcointroini
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
