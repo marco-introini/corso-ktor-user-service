942f4ced47ed2d607d691da9dda2196129eb1c75
Marco "Mint" Introini <mint.dev@pm.me>
2025-03-16 16:10:35 +0100

    Add HikariCP dependency to support database connection pooling
    Included the HikariCP library in `build.gradle.kts` for managing database connection pooling. Updated `libs.versions.toml` to define the HikariCP version and reference the library within the project dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
 
+    // HikariCP
+    implementation(libs.hikari.cp)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
