a8e843e03529f1de60605694c0bb2ada0685a783
Marco "Mint" Introini <mint.dev@pm.me>
2025-03-16 16:36:55 +0100

    Refactor database configuration and initialization.
    Replaced hardcoded database credentials with environment variable-based configuration in `application.yaml`. Added HikariCP connection pooling for better database performance, and updated the application to use a dedicated `getDataSource` function for database connection management.

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,14 +1,40 @@
 package it.marcointroini
 
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.application.*
+import io.ktor.server.config.ApplicationConfig
+import kotlin.time.Duration.Companion.seconds
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    val host = environment.config.property( "ktor.deployment.host").getString()
-    val port = environment.config.property( "ktor.deployment.port").getString()
+    val host = environment.config.getProperty("ktor.deployment.host")
+    val port = environment.config.getProperty("ktor.deployment.port").toInt()
 
     println("Running on $host:$port")
+
+    val dataSource = getDataSource(environment.config)
+    dataSource.connection
+}
+
+private fun ApplicationConfig.getProperty(path: String): String = property(path).getString()
+
+private fun getDataSource(config: ApplicationConfig): HikariDataSource {
+    val dbUrl = config.getProperty("database.url")
+    val dbUser = config.getProperty("database.user")
+    val dbPassword = config.getProperty("database.password")
+
+    return HikariDataSource().apply {
+        jdbcUrl = dbUrl
+        username = dbUser
+        password = dbPassword
+        driverClassName = "org.postgresql.Driver"
+        maximumPoolSize = 3
+        minimumIdle = 2
+        idleTimeout = 10.seconds.inWholeMilliseconds
+        connectionTimeout = 30.seconds.inWholeMilliseconds
+        maxLifetime = 30.seconds.inWholeMilliseconds
+    }
 }
